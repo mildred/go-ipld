b82e4e74145d89b510a03c3663218dc87c926ed4
Mildred Ki'Lya <mildred-pub.git@mildred.fr>
2016-02-17 09:20:20 +0100

    links: implement ReadLinks()
    ReadLink() traverse a NodeReader and for each link found, add it to the
    link slice given as argument. Links must have ipld type annotations to
    mark special and extra fields.
    
    # Conflicts:
    #	links/links.go

---
+++ links/links_test.go
@@ -0,0 +1,141 @@
+package links
+
+import (
+	"testing"
+
+	memory "github.com/ipfs/go-ipld/memory"
+	mh "github.com/jbenet/go-multihash"
+	assrt "github.com/mildred/assrt"
+)
+
+const (
+	h1 string = "QmZku7P7KeeHAnwMr6c4HveYfMzmtVinNXzibkiNbfDbPa"
+	h2 string = "QmZku7P7KeeHAnwMr6c4HveYfMzmtVinNXzibkiNbfDbPb"
+	h3 string = "QmZku7P7KeeHAnwMr6c4HveYfMzmtVinNXzibkiNbfDbPc"
+)
+
+var (
+	mh1 mh.Multihash = FromB58String(h1)
+	mh2 mh.Multihash = FromB58String(h2)
+	mh3 mh.Multihash = FromB58String(h3)
+)
+
+func FromB58String(s string) (h mh.Multihash) {
+	h, err := mh.FromB58String(s)
+	if err != nil {
+		panic(err)
+	}
+	return h
+}
+
+type Link1 struct {
+	Hash []byte `ipld:"multihash"` //mh.Multihash
+	Name string `ipld:"name"`
+	Size uint64 `ipld:"key:size"`
+}
+
+func TestParsing(t *testing.T) {
+	a := assrt.NewAssert(t)
+
+	n := memory.Node{
+		"foo": memory.Node{
+			LinkKey: h1,
+			"size":  3,
+		},
+		"bar": memory.Node{
+			"baz": memory.Node{
+				LinkKey: h2,
+				"size":  42,
+				"boo": memory.Node{
+					LinkKey: h3,
+				},
+			},
+		},
+	}
+
+	var links []Link1
+
+	err := ReadLinks(n, &links)
+	a.Nil(err)
+
+	a.Equal([]Link1{
+		Link1{
+			Name: "boo",
+			Hash: mh3,
+			Size: 0,
+		},
+		Link1{
+			Name: "baz",
+			Hash: mh2,
+			Size: 42,
+		},
+		Link1{
+			Name: "foo",
+			Hash: mh1,
+			Size: 3,
+		},
+	}, links)
+}
+
+type Link2 struct {
+	BaseLink
+	Size uint64 `ipld:"key:size"`
+}
+
+func TestParsing2(t *testing.T) {
+	a := assrt.NewAssert(t)
+
+	n := memory.Node{
+		"foo": memory.Node{
+			LinkKey: h1,
+			"size":  3,
+		},
+		"bar": memory.Node{
+			"baz": memory.Node{
+				LinkKey: h2,
+				"size":  42,
+				"boo": memory.Node{
+					LinkKey: h3,
+				},
+			},
+		},
+	}
+
+	var links []Link2
+
+	err := ReadLinks(n, &links)
+	a.Nil(err)
+
+	a.Equal([]Link2{
+		Link2{
+			BaseLink: BaseLink{
+				Name:       "boo",
+				Hash:       mh3,
+				Link:       h3,
+				StringPath: []string{"bar", "baz", "boo"},
+				Path:       []interface{}{"bar", "baz", "boo"},
+			},
+			Size: 0,
+		},
+		Link2{
+			BaseLink: BaseLink{
+				Name:       "baz",
+				Hash:       mh2,
+				Link:       h2,
+				StringPath: []string{"bar", "baz"},
+				Path:       []interface{}{"bar", "baz"},
+			},
+			Size: 42,
+		},
+		Link2{
+			BaseLink: BaseLink{
+				Name:       "foo",
+				Hash:       mh1,
+				Link:       h1,
+				StringPath: []string{"foo"},
+				Path:       []interface{}{"foo"},
+			},
+			Size: 3,
+		},
+	}, links)
+}
